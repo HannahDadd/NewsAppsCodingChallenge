9d82fca9022762c41e5d0a40abfc96c5e874fcc7
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-23 08:08:57 +0100

    MODEL(headlines-model): Headline and News Feed entities

---
+++ NewsAppsCodingChallenge/Entities/Headline.swift
@@ -0,0 +1,14 @@
+//
+//  Headline.swift
+//  NewsAppsCodingChallenge
+//
+//  Created by Hannah Billingsley-Dadd on 23/07/2022.
+//
+
+import Foundation
+
+struct Headline {
+	let headline: String
+	let updated: Int
+	let introduction: String
+}
---
+++ NewsAppsCodingChallenge/Entities/NewsFeed.swift
@@ -0,0 +1,12 @@
+//
+//  NewsFeed.swift
+//  NewsAppsCodingChallenge
+//
+//  Created by Hannah Billingsley-Dadd on 23/07/2022.
+//
+
+import Foundation
+
+struct NewsFeed {
+	let headlines: [Headline]
+}
