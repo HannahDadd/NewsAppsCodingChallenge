5c371b28829b7f42daddfef8159bb1e138911b80
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-23 08:06:23 +0100

    TEST(headlines-model): test model will decode correctly

--- NewsAppsCodingChallengeTests/Entities/HeadlinesDecodableTests.swift
+++ NewsAppsCodingChallengeTests/Entities/HeadlinesDecodableTests.swift
@@ -12,12 +12,26 @@ class HeadlinesDecodableTests: XCTestCase {
 
 	func testWarningsResponseIsDecodedCorrectly() throws {
 
-			// Given a JSON Headlines response
+		// Given a JSON Headlines response
+		let stubbedHeadlinesData = getDataForJsonFile("headlines-success-reponse")
 
+		// When the response is decoded to the News Feed entity
+		let decoder = JSONDecoder()
+		let dateFormatter = DateFormatter()
+		dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+		dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+		decoder.dateDecodingStrategy = .formatted(dateFormatter)
+		let newsFeed: NewsFeed = try! decoder.decode(NewsFeed.self, from: stubbedHeadlinesData)
 
-			// When the response is decoded to the News Feed entity
+		// Then the correct fields are filled in
+		XCTAssertEqual(newsFeed.headlines.count, 9)
 
-			// Then the correct fields are filled in
-			
-		}
+		XCTAssertEqual(newsFeed.headlines[0].headline, "Rare angel sharks found living off Wales")
+		XCTAssertEqual(newsFeed.headlines[0].updated, Date(timeIntervalSinceReferenceDate: TimeInterval(1448401928)))
+		XCTAssertEqual(newsFeed.headlines[0].introduction, "Scientists have found evidence that one of the world's rarest sharks is alive and well, living off the Welsh coast.")
+
+		XCTAssertEqual(newsFeed.headlines[8].headline, "What dinosaur footprints tell us")
+		XCTAssertEqual(newsFeed.headlines[8].updated, Date(timeIntervalSinceReferenceDate: TimeInterval(1546300000)))
+		XCTAssertEqual(newsFeed.headlines[8].introduction, "While skeletons show us what dinosaurs looked like, their footprints can reveal how they moved and behaved. Dr Peter Falkingham, a biologist at Liverpool John Moores University in the UK, analyses dinosaur tracks from around the world.")
+	}
 }
