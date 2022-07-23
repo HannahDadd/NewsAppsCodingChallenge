db1e9132802bd17cde42d69a71caa85462d8e277
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-23 07:15:20 +0100

    Initial Commit

---
+++ NewsAppsCodingChallenge/NewsAppsCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppsCodingChallengeApp.swift
+//  NewsAppsCodingChallenge
+//
+//  Created by Hannah Billingsley-Dadd on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppsCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
