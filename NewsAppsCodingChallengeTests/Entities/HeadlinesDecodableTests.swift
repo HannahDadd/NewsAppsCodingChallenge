291baeaf5a4797b1a62a6875a3216a1ebda42155
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-23 07:51:45 +0100

    TEST(headlines-model): test class

---
+++ NewsAppsCodingChallengeTests/Entities/HeadlinesDecodableTests.swift
@@ -0,0 +1,23 @@
+//
+//  HeadlinesDecodableTests.swift
+//  NewsAppsCodingChallengeTests
+//
+//  Created by Hannah Billingsley-Dadd on 23/07/2022.
+//
+
+import XCTest
+@testable import NewsAppsCodingChallenge
+
+class HeadlinesDecodableTests: XCTestCase {
+
+	func testWarningsResponseIsDecodedCorrectly() throws {
+
+			// Given a JSON Headlines response
+
+
+			// When the response is decoded to the News Feed entity
+
+			// Then the correct fields are filled in
+			
+		}
+}
