b87386cd2668ab768353e230cad35460421657f2
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-24 19:12:10 +0100

    INTERACTOR(article-interactor): article interactor

---
+++ NewsAppsCodingChallenge/Interactors/ArticleInteractor.swift
@@ -0,0 +1,12 @@
+//
+//  ArticleInteractor.swift
+//  NewsAppsCodingChallenge
+//
+//  Created by Hannah Billingsley-Dadd on 24/07/2022.
+//
+
+import Foundation
+
+struct ArticleInteractor {
+	let headline: Headline
+}
