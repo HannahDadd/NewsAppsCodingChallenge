0eb3721bd5288746da675fe4e3d7ff2ca019fd68
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-24 18:26:35 +0100

    TEST(HeadlinePresenter): headline presenter unit tests

---
+++ NewsAppsCodingChallengeTests/Presenters/HeadlinePresenterTests.swift
@@ -0,0 +1,30 @@
+//
+//  HeadlinePresenterTests.swift
+//  NewsAppsCodingChallengeTests
+//
+//  Created by Hannah Billingsley-Dadd on 24/07/2022.
+//
+
+import XCTest
+@testable import NewsAppsCodingChallenge
+
+class HeadlinePresenterTests: XCTestCase {
+
+	func testPresenterFormatsDateCorrectly() {
+
+		// Given a HeadlinePresenter with a Headline
+		let headlinePresenter = HeadlinePresenter(headline: NewsFeedStubbedData.getSampleNewsFeed().headlines[0])
+
+		// Then the date is formatted as required by the designs
+		XCTAssertEqual(headlinePresenter.updatedTimestamp, "24 November 2015")
+	}
+
+	func testPresenterOutputsHeadlineTextUnchanged() {
+
+		// Given a HeadlinePresenter with a Headline
+		let headlinePresenter = HeadlinePresenter(headline: NewsFeedStubbedData.getSampleNewsFeed().headlines[0])
+
+		// Then the headline is unchanged
+		XCTAssertEqual(headlinePresenter.headline, "Rare angel sharks found living off Wales")
+	}
+}
