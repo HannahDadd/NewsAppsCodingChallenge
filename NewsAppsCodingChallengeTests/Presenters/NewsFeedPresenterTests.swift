05b91302a66056e1049a7ab963f148a4a01c9689
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-25 08:44:48 +0100

    REFACTOR(bug-flaky-test): add delay to failing test

--- NewsAppsCodingChallengeTests/Presenters/NewsFeedPresenterTests.swift
+++ NewsAppsCodingChallengeTests/Presenters/NewsFeedPresenterTests.swift
@@ -18,19 +18,26 @@ class NewsFeedPresenterTests: XCTestCase {
 
 		// When the NewsFeedPresenter asks the interactor for data
 		newsFeedPresenter.fetchNewsFeed()
+		let exp = expectation(description: "Test after 5 seconds")
+		let result = XCTWaiter.wait(for: [exp], timeout: 5.0)
 
 		// Then the news feed result is populated with HeadlinePresenters
-		// There is no need to test the failure path here as the interactor handles
-		// failure- presenter should just spit out result of interactor
-		switch newsFeedPresenter.newsFeedResult {
-		case .success(let newsFeed):
-			XCTAssertEqual(newsFeed, [
-				HeadlinePresenter(headline: NewsFeedStubbedData.getSampleNewsFeed().headlines[0]),
-				HeadlinePresenter(headline: NewsFeedStubbedData.getSampleNewsFeed().headlines[1])])
-		case .failure(_):
-			XCTFail("Interactor should have data")
-		case .none:
-			XCTFail("Interactor should have data")
+		if result == XCTWaiter.Result.timedOut {
+
+			// There is no need to test the failure path here as the interactor handles
+			// failure- presenter should just spit out result of interactor
+			switch newsFeedPresenter.newsFeedResult {
+			case .success(let newsFeed):
+				XCTAssertEqual(newsFeed, [
+					HeadlinePresenter(headline: NewsFeedStubbedData.getSampleNewsFeed().headlines[0]),
+					HeadlinePresenter(headline: NewsFeedStubbedData.getSampleNewsFeed().headlines[1])])
+			case .failure(_):
+				XCTFail("Interactor should have data")
+			case .none:
+				XCTFail("Interactor should have data")
+			}
+		} else {
+			XCTFail("Delay interrupted")
 		}
 	}
 
