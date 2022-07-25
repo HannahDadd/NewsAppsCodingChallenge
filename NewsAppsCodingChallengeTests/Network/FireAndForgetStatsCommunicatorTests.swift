776203d25c82381d37cbe659fd00b6e2a9405406
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-25 07:55:25 +0100

    TEST(network-stats): test url can be formatted

---
+++ NewsAppsCodingChallengeTests/Network/FireAndForgetStatsCommunicatorTests.swift
@@ -0,0 +1,24 @@
+//
+//  StatsCommunicator.swift
+//  NewsAppsCodingChallengeTests
+//
+//  Created by Hannah Billingsley-Dadd on 25/07/2022.
+//
+
+import XCTest
+@testable import NewsAppsCodingChallenge
+
+class FireAndForgetStatsCommunicatorTests: XCTestCase {
+
+	func testFireAndForgetStatsCommunicatorCanFormatURL() {
+
+		// Given a FireAndForgetStatsCommunicator and paramters
+		let fireAndForgetStatsCommunicator = FireAndForgetStatsCommunicator()
+
+		// When the FireAndForgetStatsCommunicator builds a url
+		let urlString = fireAndForgetStatsCommunicator.createURL(parameters: ["event": "load", "data": "100"])
+
+		// Then the correct URLString is created
+		XCTAssertEqual(urlString, "https://raw.githubusercontent.com/bbc/news-apps-ios-coding-challenge/master/analytics?event=load&data=100")
+	}
+}
